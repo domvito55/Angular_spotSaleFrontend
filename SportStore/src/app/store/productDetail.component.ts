46fafd1ed32fa5aab769c38b74d0902c0c65b007
domvito55 <domvito55@gmail.com>
2022-11-11 05:16:48 -0500

    BotaoPraEdit

--- SportStore/src/app/store/productDetail.component.ts
+++ SportStore/src/app/store/productDetail.component.ts
@@ -1,17 +1,34 @@
 import { Component } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
 import { NgForm } from "@angular/forms";
 import { ProductRepository } from "../model/product.repository";
 import { Product } from "../model/product.model";
+
 @Component({
     templateUrl: "productDetail.component.html",
     styleUrls: ["productDetail.component.css"]
 })
+
 export class ProductDetailComponent {
+    editing: boolean = false;
     productSent: boolean = false;
     submitted: boolean = false;
     public product: Product  = new Product();
 
-    constructor(public repository: ProductRepository) {}
+    constructor(private repository: ProductRepository,
+        private router: Router,
+        activeRoute: ActivatedRoute) {
+            this.editing = activeRoute.snapshot.params["mode"] == "edit";
+            if (this.editing) {
+                Object.assign(this.product,
+                    repository.getProduct(activeRoute.snapshot.params["id"]));
+            }
+    }
+    save(form: NgForm) {
+        this.repository.saveProduct(this.product);
+        this.router.navigateByUrl("/admin/main/products");
+    }
+
     submitProduct(form: NgForm) {
         this.submitted = true;
         if (form.valid) {
