a46df881fed34ba423c12e1db43c9e9c73b6fafe
domvito55 <domvito55@gmail.com>
2022-11-11 01:27:21 -0500

    FuncionandoRestful

--- SportStore/src/app/model/rest.datasource.ts
+++ SportStore/src/app/model/rest.datasource.ts
@@ -6,13 +6,25 @@ import { Product } from "./product.model";
 const PROTOCOL = "http";
 const PORT = 3500;
 
+
 @Injectable()
 export class RestDataSource {
   baseUrl: string;
   constructor(private http: HttpClient) {
-    this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
+//  this.baseUrl = "https://groupproject-comp229-2022.herokuapp.com/advertisement/";
+    this.baseUrl = `${PROTOCOL}://orca-app-yod9h.ondigitalocean.app/advertisement/`;
+//    this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
+console.log("=== location: " + this.baseUrl)
   }
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseUrl + "products");
+//    return this.http.get<Product[]>(this.baseUrl + "products");
+    return this.sendRequest<Product[]>("GET", this.baseUrl);
   }
+  private sendRequest<T>(verb: string, url: string, body?: Product)
+           : Observable<T> {
+        return this.http.request<T>(verb, url, {
+            body: body,
+            withCredentials: false
+        });
+    }
 }
