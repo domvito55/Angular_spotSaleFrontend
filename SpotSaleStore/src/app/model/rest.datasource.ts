6f9012b94d40ceecbdc042aa0031208fbcda77da
domvito55 <domvito55@gmail.com>
2022-11-19 15:34:00 -0500

    ProfessorHelp
    Some dependecies were updated, but we still have some problems

--- SpotSaleStore/src/app/model/rest.datasource.ts
+++ SpotSaleStore/src/app/model/rest.datasource.ts
@@ -11,8 +11,8 @@ const PORT = 3500;
 export class RestDataSource {
   baseUrl: string;
   constructor(private http: HttpClient) {
-  this.baseUrl = "https://groupproject-comp229-2022.herokuapp.com/advertisement/";
-//    this.baseUrl = `${PROTOCOL}://orca-app-yod9h.ondigitalocean.app/advertisement/`;
+//  this.baseUrl = "https://groupproject-comp229-2022.herokuapp.com/advertisement/";
+    this.baseUrl = `${PROTOCOL}://orca-app-yod9h.ondigitalocean.app/advertisement/`;
 //    this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
   }
   getProducts(): Observable<Product[]> {
