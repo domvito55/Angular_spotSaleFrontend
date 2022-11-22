9aa2e931a8518f7d0c62377eb6d12cca7c57afc2
Dinu <dinugherciu93@gmail.com>
2022-11-22 16:36:33 -0500

    fixing the sign-up

--- SpotSaleStore/src/app/store/productDetail.component.ts
+++ SpotSaleStore/src/app/store/productDetail.component.ts
@@ -18,25 +18,49 @@ export class ProductDetailComponent {
     pageTitle: string = 'Create';
     editing: boolean = false;
     submitted: boolean = false;
-    public product: Product  = new Product();
+    public product: Product = new Product();
 
     constructor(private repository: ProductRepository,
         private router: Router,
         activeRoute: ActivatedRoute) {
-            //decide if the page is for edit or create
-            this.editing = activeRoute.snapshot.params["mode"] == "edit";
-            if (this.editing) {
-                Object.assign(this.product,
-                    repository.getProduct(activeRoute.snapshot.params["id"]));
-            }
+
+        //decide if the page is for edit or create
+        this.editing = activeRoute.snapshot.params["mode"] == "edit";
+
+        if (this.editing) {
+            Object.assign(this.product,
+                repository.getProduct(activeRoute.snapshot.params["id"]));
+        }
+        // Delete
+        if (activeRoute.snapshot.params["mode"] == "delete") {
+            this.deleteProduct(activeRoute.snapshot.params["id"]);
+        }
+
+        this.editing = activeRoute.snapshot.params["mode"] == "edit";
+
+        // Edit
+        if (this.editing) {
+            this.product = this.repository.getProduct(activeRoute.snapshot.params["id"]);
+        }
     }
 
-/* example of validation to be used in future release
+
     save(form: NgForm) {
         this.repository.saveProduct(this.product);
-        this.router.navigateByUrl("/admin/main/products");
+        this.router.navigateByUrl("/");
     }
-*/
+
+    private deleteProduct(id: string) {
+        this.repository.deleteProduct(id);
+        this.router.navigateByUrl("/");
+    }
+
+    /* example of validation to be used in future release
+        save(form: NgForm) {
+            this.repository.saveProduct(this.product);
+            this.router.navigateByUrl("/admin/main/products");
+        }
+    */
     //Saving (created or adited ad)
     submitProduct(form: NgForm) {
         this.submitted = true;
