9479b388768584c7fbe6dc73729d8a1c7d55f4db
Matheus Teixeira <domvito55@gmail.com>
2023-04-13 19:49:56 -0400

    No significant changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiUrl:"https://spotsalebackend.uk.r.appspot.com/"
+  //apiUrl:"http://localhost:3000/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl:"https://spotsalebackend.uk.r.appspot.com/"
-  // apiUrl:"http://localhost:3000/"
+  //apiUrl:"http://localhost:3000"
 };
 
 /*
